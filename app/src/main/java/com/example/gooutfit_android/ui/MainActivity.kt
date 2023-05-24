3dc1970f96023b93a1076ecdc089e1b15e3e6120
dededwiwiraputra <dededwiwiraputra17@gmail.com>
2023-05-24 10:00:18 +0700

    First Commit

---
+++ app/src/main/java/com/example/gooutfit_android/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gooutfit_android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gooutfit_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
